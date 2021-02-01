0fa2313317f759acb7e824af1182f2842839f35d
yuki-snow1823 <magic.iroha@gmail.com>
2021-02-01 22:48:14 +0900

    filter

--- chapter1-4/main.js
+++ chapter1-4/main.js
@@ -2,8 +2,13 @@ var app = new Vue({
   el: "#app",
   data: {
     price: 0,
-    stock: 0,
+    stock: 1,
   },
+  filters: {
+    test: function (val) {
+      return val + 5;
+    }
+  }
 });
 app.stock = 1;
 app.price = 1000;
