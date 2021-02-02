9a7baeb9a88bb3370e2eb3253abaca47ad3cb968
yuki-snow1823 <magic.iroha@gmail.com>
2021-02-02 11:39:23 +0900

    watchオプション

--- chapter1-4/main.js
+++ chapter1-4/main.js
@@ -2,13 +2,26 @@ var app = new Vue({
   el: "#app",
   data: {
     price: 0,
-    stock: 1,
+    stock: 5,
   },
   filters: {
-    test: function (val) {
+    test: function(val) {
       return val + 5;
+    },
+  },
+  methods: {
+    onDeleteItem: function() {
+      this.stock--;
+    },
+  },
+  watch: {
+    // プロパティを監視
+    stock: function (newStock, oldStock) {
+      console.log(newStock);
+      console.log(oldStock);
+      if (newStock == 0) {
+        this.price = 1000000;
+      }
     }
   }
 });
-app.stock = 1;
-app.price = 1000;
